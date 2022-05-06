94a16b985d2edc86761da9856008ec8152beb291
21107093 <105062647+21107093@users.noreply.github.com>
2022-05-06 20:04:21 +0530

    Add files via upload

---
+++ assignment 2/question2.py
@@ -0,0 +1,7 @@
+name = "YASH PAREEK"
+SID = "21107093"
+CGPA = "9.9"
+department_name = "MECHANICAL"
+
+
+print("Hey,",name, "Here!\nMy SID is", SID,"\nI am from", department_name, "department and my CGPA is", CGPA )
