486c2f268768e77605fa96dc16f02e60baa80d54
21107093 <105062647+21107093@users.noreply.github.com>
2022-05-12 19:50:55 +0530

    Add files via upload

---
+++ Assignment 3 output.py
@@ -0,0 +1,51 @@
+Q1
+Enter a number:20
+Binary equivalent of 20 is 10100
+Q2
+Enter expression:6-3+99/2
+6-3+99/2 = 52.5
+Q3
+Enter an integer:3
+a) 35
+b) 3.0
+enter radius:6
+c) 452.3893421169302
+Enter angle 1:20
+Enter angle 1:70
+d) 2.143041422151242
+Enter x coordinate of first point:8
+Enter y coordinate of first point:4
+Enter x coordinate of second point:3
+Enter y coordinate of second point:-1
+e) 1.0
+Q4
+4.(a)
+0
+1
+2
+3
+4
+4.(b)
+3
+4
+5
+6
+7
+8
+9
+4.(c)
+4
+7
+10
+4.(d)
+15
+13
+11
+9
+7
+4.(e)
+Q5
+Enter number of carbon atoms in molecule:2
+Enter number of hydrogen atoms in molecule:4
+Enter number of oxygen atoms in molecule:2
+The molecular weight of the compound is 60.051959999999994
